57b8b6a364be7f1bc9a5c8905231385b00a4b93d
Joshua Brewster (Bubba Skrimp) <brewster.joshua1@gmail.com>
2023-11-12 17:17:39 -0800

    fixed apikey setting

--- gptdocument.js
+++ gptdocument.js
@@ -4,15 +4,18 @@ import {generateDocumentation, args} from './utils.js'
 
 // Convert comma-separated string to array
 const toArray = (str) => str ? str.split(',').map(item => item.trim()) : [];
-
-// CLI usage to call generateDocumentation
-generateDocumentation(
-    args.entryPoint ? args.entryPoint : process.cwd(), 
-    args.initialFiles ? toArray(args.initialFiles) : undefined, 
-    args.extensions ? toArray(args.extensions) : ['.js', '.ts', '.mjs', '.jsx', '.tsx'], 
-    args.excluded ? toArray(args.excluded) : ['dist','node_modules'],
-    null// onProgress=(partialResponse, lastLength) => {
-    //     console.log(partialResponse.text.substring(lastLength));
-    //     curLength = partialResponse.text.length;
-    // },
-);
+if(args.apiKey) {
+    console.log("Setting api key (saved to this library's install location)");
+} else {
+    // CLI usage to call generateDocumentation
+    generateDocumentation(
+        args.entryPoint ? args.entryPoint : process.cwd(), 
+        args.initialFiles ? toArray(args.initialFiles) : undefined, 
+        args.extensions ? toArray(args.extensions) : ['.js', '.ts', '.mjs', '.jsx', '.tsx'], 
+        args.excluded ? toArray(args.excluded) : ['dist','node_modules'],
+        null// onProgress=(partialResponse, lastLength) => {
+        //     console.log(partialResponse.text.substring(lastLength));
+        //     curLength = partialResponse.text.length;
+        // },
+    );
+}
--- utils.js
+++ utils.js
@@ -1,19 +1,26 @@
 import fs from 'fs'
 import path from 'path'
+import * as url from 'url';
 import { ChatGPTAPI } from 'chatgpt'
 
 let apiKey = ""; //use this if not using CLI
 
-const systemMessage = `You are an expert developer tasked with reading and documenting source code for creating developer documentation that assumes the reader is an entry 
-to mid-level developer who is familiar with programming languages and conventions and techniques. 
-You are fed app or library source code starting with the most important files and you will document then build on the context of each file to produce even better, well-written and cohesive markdown documents for each file you are given. Weave together the explanations best you can as files add up. 
-This will include a high level summary of the purpose of the file and then document all of the methods and their inputs, outputs, and general use case. If it's clearly not meant as an API, you can summarize into 1 or 2 paragraphs or a few bullet points.
+const systemMessage = `As an expert developer, your task is to analyze, understand, and document source code for comprehensive developer documentation. Your audience is entry to mid-level developers with a foundational grasp of programming concepts, languages, and techniques.
 
-Your output will be parsed into a markdown file, so write your response assuming it will be readable as a .md file to be used out of the gate for developer documentation, so be very concise and assume your reader is very intelligent!
-`;
+You will receive source code from applications or libraries, starting with the most critical files. Your documentation should build upon each file's context, forming cohesive, well-articulated Markdown documents. Aim for clarity and conciseness, tailoring your explanations to your intelligent audience.
 
-const API_KEY_FILE = path.join(import.meta.url, 'apiKey.txt');
+For each file, provide:
 
+A high-level summary explaining its purpose and role.
+Detailed documentation of all methods, including:
+Input parameters.
+Output values.
+General use cases and examples.
+If a file does not serve as an API and its functionality is straightforward, a concise summary of one or two paragraphs, or a few bullet points, will suffice.
+
+Your output will be directly converted into a Markdown (.md) file. Therefore, ensure your documentation is structured for immediate use as developer documentation, with clarity and precision as its cornerstones.`;
+
+const API_KEY_FILE = path.join(url.fileURLToPath(new URL('.', import.meta.url)), 'apiKey.txt');
 // Function to set the API key and save it to a file
 export function setApiKey(apiKey) {
     fs.writeFileSync(API_KEY_FILE, apiKey, 'utf8');
@@ -24,7 +31,6 @@ export function getApiKey() {
     if (fs.existsSync(API_KEY_FILE)) {
         return fs.readFileSync(API_KEY_FILE, 'utf8');
     } else return apiKey;
-    return null;
 }
 
 export const get = (args=process.argv) => {
@@ -53,9 +59,9 @@ export const args = get();
 // Set API key if provided
 if (args.apiKey) {
   apiKey = args.apiKey;
-}
+  setApiKey(args.apiKey);
+} else apiKey = getApiKey();
 
-apiKey = getApiKey();
 
 console.log("Initializing ChatGPTAPI");
 
